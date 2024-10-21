e97ab40d7b5fe372e490fa502ce25066dd6a1d1a
Dang Quoc Anh Duy <90438687+DominicDang@users.noreply.github.com>
2024-10-21 20:00:24 +0700

    Solved part C challenges

--- Case Study 2: Pizza Runner/ingredients_optimization.sql
+++ Case Study 2: Pizza Runner/ingredients_optimization.sql
@@ -43,13 +43,61 @@ ALTER COLUMN pickup_time TYPE timestamp with time zone USING pickup_time::timest
 
 -- C. Ingredient Optimisation
 -- 1. What are the standard ingredients for each pizza?
+
+SELECT *
+FROM pizza_runner.pizza_toppings;
+
 -- 2. What was the most commonly added extra?
+
+WITH toppings AS (SELECT pizza_id, 
+	   REGEXP_SPLIT_TO_TABLE(toppings, '[,\s]+')::INTEGER AS topping_id
+FROM pizza_runner.pizza_recipes)
+
+SELECT topping_id, pt.topping_name, COUNT(*) AS total_frequency
+FROM toppings t
+INNER JOIN pizza_toppings pt
+USING (topping_id)
+GROUP BY t.topping_id, pt.topping_name
+ORDER BY total_frequency DESC;
+
 -- 3. What was the most common exclusion?
--- 4. Generate an order item for each record in the customers_orders table in the format of one of the following:
--- Meat Lovers
--- Meat Lovers - Exclude Beef
--- Meat Lovers - Extra Bacon
--- Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
--- 5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients
--- For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
--- 6. What is the total quantity of each ingredient used in all delivered pizzas sorted by most frequent first?
+
+WITH toppings_excluded AS (SELECT pizza_id,
+	   REGEXP_SPLIT_TO_TABLE(exclusions, '[,\s]+'):: INTEGER AS topping_id
+FROM customer_orders_temp)
+
+SELECT topping_id, COUNT(*) AS frequency
+FROM toppings_excluded
+GROUP BY topping_id;
+
+-- 4. Generate an order item for each record in the customers_orders table in the format of one of the following: Meat Lovers Meat Lovers - Exclude Beef Meat Lovers - Extra Bacon Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
+
+WITH toppings_data AS 
+	(SELECT order_id, 
+			REGEXP_SPLIT_TO_TABLE(exclusions, '[,\s]+'):: INTEGER AS exclusions,
+			REGEXP_SPLIT_TO_TABLE(extras, '[,\s]+'):: INTEGER AS extras
+FROM customer_orders_temp)
+
+SELECT order_id, 
+	   CASE WHEN exclusions = 3 THEN 'Meat Lovers - Exclude Beef'
+       		WHEN extras = 1 THEN 'Meat Lovers - Extra Bacon'
+            WHEN exclusions = 4 THEN 'Meat Lovers - Exclude Cheese'
+            WHEN extras = 6 AND extras = 9 THEN 'Bacon - Extra Mushrooms, Peppers'
+       ELSE 'Meat Lovers' END AS order_type
+FROM toppings_data
+GROUP BY order_id, exclusions, extras
+
+-- 5. Generate an alphabetically ordered comma separated ingredient list for each pizza order from the customer_orders table and add a 2x in front of any relevant ingredients For example: "Meat Lovers: 2xBacon, Beef, ... , Salami" 
+
+SELECT 
+    pizza_type, 
+    GROUP_CONCAT(
+        CASE 
+            WHEN quantity = 2 THEN CONCAT('2x', ingredient)
+            ELSE ingredient
+        END
+        ORDER BY ingredient ASC
+        SEPARATOR ', '
+    ) AS ingredients_list
+FROM customer_orders_temp
+GROUP BY pizza_type;
